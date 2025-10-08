1bf76afe3e1df8b2413d79850ec491e6a787644e
Medha30042001 <83180508+Medha30042001@users.noreply.github.com>
2025-10-08 16:18:56 +0530

    Create 06-fizzBuzz.js

---
+++ 06-fizzBuzz.js
@@ -0,0 +1,22 @@
+//my appoach
+
+function fizzBuzz(num){
+  for(let i=1; i<=num; i++){
+    if(i%3==0 && i%5==0) console.log('FizzBuzz')
+    else if(i%3==0) console.log('Fizz')
+    else if(i%5==0) console.log('Buzz')
+    else console.log(i);
+  }
+}
+
+function main(){
+  const input = require('fs').readFileSync(0, 'utf-8').trim().split('\n');
+  let testcases = parseInt(input[0], 10);
+  
+  for(let i = 1; i<=testcases; i++){
+    let num = input[i]
+    fizzBuzz(num);
+  }
+}
+
+main();
